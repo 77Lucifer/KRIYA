e40cad837d923e526bcde5540eb32839bf5eb58d
77Lucifer <lucifer17860@gmail.com>
2024-01-06 18:15:07 +0530

    fix responsive

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import logo from "./images/Kriya_bg_remove.png"
-import './Style/Nav.css'
-import { Link } from 'react-router-dom'
-import { useEffect } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useState } from 'react';
+import logo from "./images/Kriya_bg_remove.png";
+import './Style/Nav.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    const [showNav, setShowNav] = useState(false);
 
-//     uEffect(() => {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     const tl = gsap.timeline();
-
-//     gsap.from('.logo', {
-//       y: '-100%',
-//       delay: 5,
-//     });
-
-// }, []);se
+    const toggleNav = () => {
+        setShowNav(!showNav);
+    };
 
     return (
         <>
             <div className="logo">
-                <a src="/">
+                <a href="/">
                     <img src={logo} alt="" />
                 </a>
-                <ul className='nav-ul'>
+                <ul className={`nav-ul ${showNav ? 'hide' : ''}`}>
                     <li className='nav-li'><Link to='/'>Home</Link></li>
-                    <li className='nav-li'><Link to='/Collab'>Collabration</Link></li>
+                    <li className='nav-li'><Link to='/Collab'>Collaboration</Link></li>
                     <li className='nav-li'><Link to='/Creator'>Creators</Link></li>
                     <li className='nav-li'><Link to='/Contact'>Contact Us</Link></li>
-                    <span className='humbar'><i class="fa-solid fa-bars"></i></span>
                 </ul>
+                    <li className='humbar' onClick={toggleNav}>
+                        {showNav ? <i className="fa-solid fa-times"></i> : <i className="fa-solid fa-bars"></i>}
+                    </li>
             </div>
+            <nav className={showNav ? 'active' : ''}>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/Collab'>Collaboration</Link></li>
+                    <li><Link to='/Creator'>Creators</Link></li>
+                    <li><Link to='/Contact'>Contact Us</Link></li>
+                </ul>
+            </nav>
         </>
     )
 }
 
-export default Nav
+export default Nav;
